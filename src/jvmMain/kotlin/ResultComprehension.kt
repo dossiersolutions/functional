caf27c16252960ee0cc9d262d88895ce5251939e
Jakub Petrzilka <jakub.petrzilka@dossier.no>
2021-10-26 13:45:43 +0200

    Improving result comprehension
    Adding support to launch child coroutines in the attemptBuildResult block

--- src/jvmMain/kotlin/ResultComprehension.kt
+++ src/jvmMain/kotlin/ResultComprehension.kt
@@ -1,10 +1,11 @@
 package no.dossier.libraries.functional
 
 import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
 
-class AccumulatedResultContext<U, E> {
-    var lastPartialResult: Result<U, E>? = null
-    var continuation: CancellableContinuation<*>? = null
+class AccumulatedResultContext<U, E>(override val coroutineContext: CoroutineContext) : CoroutineScope {
+    lateinit var lastPartialResult: Result<U, E>
+    var continuation: CancellableContinuation<Nothing>? = null
 
     suspend operator fun <T, F: E> Result<T, F>.not(): T = when(this) {
         is Success -> this.value
@@ -28,10 +29,15 @@ class AccumulatedResultContext<U, E> {
  */
 fun <T, E> attemptBuildResult(
     block: suspend AccumulatedResultContext<T, E>.() -> Result<T, E>
-): Result<T, E> =
-    runBlocking {
-        val context = AccumulatedResultContext<T, E>()
-        launch(start = CoroutineStart.UNDISPATCHED) { context.lastPartialResult = block(context) }
-        context.continuation?.cancel()
-        context.lastPartialResult!!
+): Result<T, E> = runBlocking {
+    val context = AccumulatedResultContext<T, E>(coroutineContext)
+
+    val job = launch(start = CoroutineStart.UNDISPATCHED) {
+        context.lastPartialResult = block(context)
     }
+
+    job.join()
+
+    context.continuation?.cancel()
+    context.lastPartialResult
+}
