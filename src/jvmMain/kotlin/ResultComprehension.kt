154a8484e8d003f52cca9ac683412e72260adc82
Jakub Petrzilka <jakub.petrzilka@dossier.no>
2021-10-27 12:33:31 +0200

    Fixing Result comprehension bug
    Fixing ResultComprehension

--- src/jvmMain/kotlin/ResultComprehension.kt
+++ src/jvmMain/kotlin/ResultComprehension.kt
@@ -3,15 +3,29 @@ package no.dossier.libraries.functional
 import kotlinx.coroutines.*
 import kotlin.coroutines.CoroutineContext
 
-class AccumulatedResultContext<U, E>(override val coroutineContext: CoroutineContext) : CoroutineScope {
-    lateinit var lastPartialResult: Result<U, E>
-    var continuation: CancellableContinuation<Nothing>? = null
+/**
+ * Wrapper that holds an instance of the latest partial result of the [attemptBuildResult]
+ */
+class ResultHolder<T, E> {
+    lateinit var lastPartialResult: Result<T, E>
+}
+
+/**
+ * This is a context object used as a receiver of the [attemptBuildResult] function. It carries a
+ * CoroutineContext instance so that we can launch child coroutines in that block, and also it carries a reference
+ * to [ResultHolder] so that the latest partial result can be set in case of [Failure].
+ */
+class AccumulatedResultContext<T, E>(
+    override val coroutineContext: CoroutineContext,
+    private val resultHolder: ResultHolder<T, E>
+) : CoroutineScope {
 
     suspend operator fun <T, F: E> Result<T, F>.not(): T = when(this) {
         is Success -> this.value
-        is Failure -> suspendCancellableCoroutine {
-            lastPartialResult = this
-            continuation = it
+        is Failure ->  {
+            resultHolder.lastPartialResult = this
+            coroutineScope { cancel() }
+            awaitCancellation()
         }
     }
 }
@@ -20,9 +34,11 @@ class AccumulatedResultContext<U, E>(override val coroutineContext: CoroutineCon
  * Attempts to build an instance of [Result] by evaluating a set of partial results.
  * This emulates monad comprehensions by controlling flow via coroutines.
  *
- * The statements inside of the *block* lambda passed in are supposed to be bound via *!* operator function
- * so that they either return the actual Success value of T or in case of Failure the whole block is interrupted
- * and that particular partial result is returned
+ * Expressions inside the *block* lambda returning Result<T, E> are supposed to be bound via *!* operator function
+ * so that they either return the Success value of T, or in case of Failure the whole block is interrupted
+ * and that specific partial Failure result is returned from the whole [attemptBuildResult] block.
+ *
+ * This a simple coroutine-based implementation of Result monad comprehension inspired by Arrow library.
  *
  * @param T Success type
  * @param E Error type
@@ -30,14 +46,13 @@ class AccumulatedResultContext<U, E>(override val coroutineContext: CoroutineCon
 fun <T, E> attemptBuildResult(
     block: suspend AccumulatedResultContext<T, E>.() -> Result<T, E>
 ): Result<T, E> = runBlocking {
-    val context = AccumulatedResultContext<T, E>(coroutineContext)
+    val resultHolder = ResultHolder<T, E>()
 
-    val job = launch(start = CoroutineStart.UNDISPATCHED) {
-        context.lastPartialResult = block(context)
+    coroutineScope {
+        launch {
+            resultHolder.lastPartialResult = block(AccumulatedResultContext(coroutineContext, resultHolder))
+        }
     }
 
-    job.join()
-
-    context.continuation?.cancel()
-    context.lastPartialResult
+    resultHolder.lastPartialResult
 }
