9451ed8c8eb2ef1bfc38d13338310d01d0826053
Jakub Petrzilka <jakub.petrzilka@dossier.no>
2022-11-01 09:00:21 +0100

    Adding LICENSE file, buildscript, gradle wrapper and gitignore

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,23 @@
+group = "no.dossier.libraries"
+version = 0.1
+
+repositories {
+    mavenCentral()
+}
+
+plugins {
+    id("org.gradle.maven-publish") // no version spec needed (Gradle built-in)
+    kotlin("multiplatform") version "1.7.20"
+}
+
 kotlin {
     jvm()
     sourceSets {
         val commonMain by getting {
             dependencies {
                 implementation(kotlin("stdlib-common"))
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.5.0")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.6.4")
             }
         }
         val commonTest by getting {
@@ -20,5 +32,11 @@ kotlin {
                 implementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
             }
         }
+    }
+}
+
+publishing {
+    repositories {
+        mavenCentral()
     }
 }
